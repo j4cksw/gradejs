3b9ba7f9d3fb7f440440691b8b2d1e08cb1968f3
Prayoch Rujira <jay@odd-e.co.th>
2016-02-12 14:38:03 +0700

    fix wrong binding to input model

--- src/grade_controller.js
+++ src/grade_controller.js
@@ -1,12 +1,16 @@
 (function(){
     angular.module('GradeJS').controller('GradeController', GradeController);
 
-    function GradeController($scope){
+    GradeController.$inject = ['$scope', 'GradeCalculator'];
+
+    function GradeController($scope, gradeCalculator){
 
         $scope.executeGrade = executeGrade;
-
+        $scope.score = 0;
+        
         function executeGrade(){
-            $scope.result = "A";
+            console.log($scope.score);
+            $scope.result = gradeCalculator.calculateScore($scope.score);
         }
     }
 })();
